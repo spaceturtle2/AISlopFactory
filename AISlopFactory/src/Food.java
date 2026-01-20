c429d7486ff1a82c4062bd3ea776e40daa4dc079
mielo.symeonidis27 <mielo.symeonidis27@10.2.103.28>
2026-01-20 10:13:39 -0500

    Push..?
    Maybe the pregnancy jokes will get old after a while

--- AISlopFactory/src/Food.java
+++ AISlopFactory/src/Food.java
@@ -1,4 +1,30 @@
 public class Food
 {
-  
+	private String name;
+    private int energyIncrease;
+    private int happinessIncrease;
+    private int weightGain;
+    
+
+    public Food(String n, int e, int h, int w) {
+
+        this.name = n;
+        this.energyIncrease = e;
+        this.happinessIncrease = h;
+        this.weightGain = w;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getEnergyIncrease() {
+        return this.energyIncrease;
+    }
+    public int getHapinessIncrease() {
+    	return this.happinessIncrease;
+    }
+    public int getWeightGain() {
+    	return this.weightGain;
+    }
 }
