eb43b6d9d1f6b461c03b5c46cd076618424aa374
Spaceturtle2 <mulahowardgriffin@gmail.com>
2026-04-07 10:08:48 -0400

    hdijhsfpiowejnfcnjvs

--- AISlopFactory/src/Game2048.java
+++ AISlopFactory/src/Game2048.java
@@ -1,4 +1,100 @@
+/**
+ * Game2048.java  
+ *
+ * @author – Your name
+ * @author – Class period
+ *
+ */  
+public class Game2048
+{
+   private int[][] gameBoard;
+   private int score;
+   private int boardSize;   
+   
+   public Game2048()
+   {
+      boardSize = 4;
+      gameBoard = new int[boardSize][boardSize];
+      score = 0;
+      add2ToBoard();
+      add2ToBoard();
+   }
+   
+   public void add2ToBoard()
+   {
+      /* To be completed in Activity 2 */
+   }      
+     
+   public void moveLeft()
+   {
+      /* To be completed in Activity 3 Part A */
+   }
+   
+   public void moveRight()
+   {
+      /* To be completed in Activity 3 Part B */
+   }
+      
+   public void moveUp()
+   {
+      /* To be completed in Activity 3 Part C */
+   }
+   
+   public void moveDown()
+   {
+      /* To be completed in Activity 3 Part D */
+   }
+   
+   public void mergeLeft()
+   {
+      /* To be completed in Activity 4 Part A*/
+   }
 
-public class Game2048 {
+   public void mergeRight()
+   {
+      /* To be completed in Activity 4 Part B*/
+   }
+   
+   public void mergeUp()
+   {
+      /* To be completed in Activity 4 Part C */
+   }
 
+   public void mergeDown()
+   {
+      /* To be completed in Activity 4 Part D */
+   }
+   
+   public boolean gameOver()
+   {
+      /* To be completed in Activity 5 */
+      return false;
+   }   
+   
+   public void displayBoard()
+   {
+      System.out.println("Score: " + score);
+      System.out.println();
+      for (int row = 0; row < gameBoard.length; row++)
+      {
+         for (int col = 0; col < gameBoard[0].length; col++)
+         {
+            if (gameBoard[row][col] == 0)
+            {
+               System.out.print("-\t\t");
+            } 
+            else if (gameBoard[row][col] < 100)
+            {
+               System.out.print(gameBoard[row][col] + "\t\t");
+            }
+            else 
+            {
+               System.out.print(gameBoard[row][col] + "\t");
+            }
+         }
+         System.out.println();
+      }
+      System.out.println();
+   }
+               
 }
--- AISlopFactory/src/GameRunner.java
+++ AISlopFactory/src/GameRunner.java
@@ -1,4 +1,63 @@
-
-public class GameRunner {
-
+/**
+ * GameRunner.java  
+ *
+ * @author – Your name
+ * @author – Class period 
+ * 
+ */ 
+import java.util.Scanner; 
+public class GameRunner
+{
+   public static void main(String[] args)
+   {
+      Game2048 myGame = new Game2048();
+      Scanner kb = new Scanner(System.in);
+      String userChoice = "";
+      
+      while (!userChoice.equals("q"))
+      {
+         System.out.println();
+         myGame.displayBoard();
+         System.out.println("l - left, r - right, u - up, d - down, q - quit");
+         System.out.print("Enter your choice: ");
+         userChoice = kb.nextLine();
+         while (userChoice.length() == 0)
+         {  
+           System.out.println("Please enter something ");
+           userChoice = kb.nextLine();
+         }
+         userChoice = userChoice.substring(0,1).toLowerCase();
+         if (userChoice.equals("l"))
+         {
+            myGame.moveLeft();
+            myGame.mergeLeft();
+            myGame.moveLeft();
+         } 
+         else if (userChoice.equals("r")) 
+         {
+            myGame.moveRight();
+            myGame.mergeRight();
+            myGame.moveRight();
+         } 
+         else if (userChoice.equals("u")) 
+         {
+            myGame.moveUp();
+            myGame.mergeUp();
+            myGame.moveUp();
+         } 
+         else if (userChoice.equals("d")) 
+         {
+            myGame.moveDown();
+            myGame.mergeDown();
+            myGame.moveDown();
+         } 
+         else 
+         {
+            System.out.println("Invalid Choice!");
+            continue; // goes directly to top of loop
+         }
+         myGame.add2ToBoard();
+      }
+      myGame.displayBoard();   
+   }
 }
